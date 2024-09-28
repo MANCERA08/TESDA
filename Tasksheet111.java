6969ec92296eb67c6c578b81f7a1986ad0589e40
MANCERA08 <jb6.mamancera2024@nrgitii.edu.ph>
2024-09-28 23:20:08 +0800

    Update Tasksheet111.java

--- Tasksheet111.java
+++ Tasksheet111.java
@@ -1,9 +1,9 @@
 /**
  * The HelloWorldApp class implements an application that
- * prints "Kamusta Mundo!" to the standard output.
+ * simply prints "Kamusta Mundo!" to the standard output.
  */
 class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Kamusta Mundo!");
+        System.out.println("Kamusta Mundo!"); //Display the string
     }
 }
