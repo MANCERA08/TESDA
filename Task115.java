3ec6d3ce7a2fe6dd11ded616e3137037c69e7ec0
Ma. Angela M. Mancera <jb6.mamancera2024@nrgitii.edu.ph>
2024-10-12 18:24:17 +0800

    Create Task115.java

---
+++ Task115.java
@@ -0,0 +1,17 @@
+class Task115 {
+    public static void main(String[] args) {
+
+        String a = new String("Wow");
+        String c = "Wow!";
+        String b = a;
+        String d = c;
+        
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
+}
