d9e865f156aa241593a2d9829c9e1700307b91bf
Ma. Angela M. Mancera <jb6.mamancera2024@nrgitii.edu.ph>
2024-10-16 14:12:45 +0800

    Update Tasksheet117.java

--- Tasksheet117.java
+++ Tasksheet117.java
@@ -1,25 +1,34 @@
 import java.util.Scanner;
 
-class Tasksheet117 {
+public class Tasksheet117 {
     
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
-        int input1 = sc.nextInt();
-        int input2= sc.nextInt();
-        int input3 = sc.nextInt();
-        
-        if(input1>input2 && input1>input3) {
-            System.out.println(input1);
-            System.out.println("The first number is the largest number");
-        } else if (input2>input1 && input2>input3) {
-            System.out.println(input2);
-            System.out.println("The second number is the largest number");
-        } else if(input3>input1 && input3>input2) {
-            System.out.println(input3);
-            System.out.println("The third number is the largest number");
+
+        int num_count = 3;
+        int[] num_store = new int[num_count];
+
+        for(int i=0; i<num_count;  i++) {
+            num_store[i] =sc.nextInt();
+        }
+
+        int large_num = num_store[0];
+        boolean equal_num = true;
+
+        for(int i=1; i<num_store.length; i++) {
+            if(num_store[i]>large_num) {
+                large_num = num_store[i];
+                equal_num = false;
+            } else if(num_store[i]!=large_num) {
+                equal_num = false;
+            }
+        }
+
+        if (equal_num) {
+            System.out.println("All numbers are the same");
         } else {
-            System.out.println("All numbers are equal.");
-          }
+            System.out.println("The largest number is: " + large_num);
+        }
     }
 }
